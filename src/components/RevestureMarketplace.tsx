3d2f8a4aa2c54f951ee3dde735f6b73d6d6be50a
luxumbra <me@luxumbra.dev>
2023-09-11 03:03:07 +0100

    Pulling in listed nfts. 🥳

--- src/components/RevestureMarketplace.tsx
+++ src/components/RevestureMarketplace.tsx
@@ -1,21 +1,28 @@
-import { useActiveListings, useListings, useContract } from "@thirdweb-dev/react";
+import { useValidDirectListings, useContract } from "@thirdweb-dev/react";
 
 export const RevestureMarketplace = ({ address }: { address: string }) => {
-  const { contract } = useContract(address, "marketplace");
-  const { data: marketplaceData, isLoading, error } = useListings(contract);
+  const { contract } = useContract(address, "marketplace-v3");
+  console.log({ contract });
+
+  const { data: marketplaceData, isLoading, error } = useValidDirectListings(contract);
   console.log(marketplaceData);
   return (
     <div className="flex justify-stretch w-5xl text-center">
       {isLoading  && <div className="text-white text-center">Loading...</div>}
       {error ? <div className="text-white text-center">Error: {error.message}</div> : null}
-      {!isLoading && !error && marketplaceData ? marketplaceData?.map((listing) => (
-        <div key={listing.id} className="text-white">
-          <h2>{listing.id}</h2>
-          <h3>{listing.asset.name}</h3>
-          <p>{listing.asset.description}</p>
+
+      {marketplaceData && (
+          <div className="grid grid-cols-3 gap-4">
+          {marketplaceData.length > 0 ? marketplaceData?.map((listing) => (
+            <div key={listing.id} className="text-white">
+              <h3>{listing.asset.name}</h3>
+              <img src={listing.asset.image} alt={listing.asset.name} />
+              <p>{listing.asset.description}</p>
+            </div>
+          )) : (
+            <div className="text-white text-center">No listings found</div>
+          )}
         </div>
-      )) : (
-        <div className="text-white text-center">No listings found</div>
       )}
     </div>
   );
